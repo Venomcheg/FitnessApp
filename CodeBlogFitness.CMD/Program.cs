3431be62fb122096f542cfbc2b1e1af35e07be29
Nikita Fidyk <niknamenik@mail.ru>
2021-05-12 16:01:59 +0700

    First version

--- CodeBlogFitness.CMD/Program.cs
+++ CodeBlogFitness.CMD/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CodeBlogFitness.BL.Controller;
 
 namespace CodeBlogFitness
 {
@@ -6,7 +7,20 @@ namespace CodeBlogFitness
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Вас приветствует приложение Fitness");
+            Console.WriteLine("Введите имя пользователя");
+            var name = Console.ReadLine();
+            Console.WriteLine("Введите пол");
+            var gender = Console.ReadLine();
+            Console.WriteLine("Введите дату рождения");
+            var birthDate = DateTime.Parse(Console.ReadLine()); //TODO: Изменить
+            Console.WriteLine("Введите вес");
+            var weight = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите рост");
+            var height = double.Parse(Console.ReadLine());
+
+            var userController = new UserController(name, gender, birthDate, weight, height);
+            userController.Save();
         }
     }
 }
